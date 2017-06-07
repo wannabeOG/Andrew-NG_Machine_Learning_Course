29263fe37fd0709768a8db3c3de6f1792751a96c
Sai Himal Allu <saihimallu1010@gmail.com>
2017-06-07 11:58:35 +0530

    Update normalEqn.m

--- ex1/normalEqn.m
+++ ex1/normalEqn.m
@@ -9,7 +9,7 @@ theta = zeros(size(X, 2), 1);
 % Instructions: Complete the code to compute the closed form solution
 %               to linear regression and put the result in theta.
 %
-
+theta=pinv(X'*X)*X'*y;
 % ---------------------- Sample Solution ----------------------
 
 
