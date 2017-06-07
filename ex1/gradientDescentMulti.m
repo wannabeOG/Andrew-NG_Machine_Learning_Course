173612e974d847b2fed8df6a3cf6bf654a893f93
Sai Himal Allu <saihimallu1010@gmail.com>
2017-06-07 11:54:04 +0530

    Update gradientDescentMulti.m
    So basically this is the same code as the one being used for linear regression. Stress has been laid about having a more general solution

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -16,7 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+%initializations
+n=length(theta);
+grad=zeros(m,1);
+sum=zeros(n,1);
+%calcualations
+grad=(X*theta-y);
+sum=(X')*grad;
+theta=theta-(alpha)*(sum/m);
 
 
 
