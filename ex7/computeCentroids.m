548102dedb1b5d06fa7cd66212e4c03bf3b7a66c
Sai Himal Allu <saihimallu1010@gmail.com>
2017-06-24 09:38:57 +0800

    Unsupervised Learning
    K-Means algo and PCA

---
+++ ex7/computeCentroids.m
@@ -0,0 +1,50 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+for i=1:K
+  % Return a logical array 
+  a=(idx==i);
+  % Keep only the true elements of the logical array 
+  b=X.*a;
+  % Delete all 0 rows
+  b(~any(b,2),:)=[];
+  % Store the mean value in the centroid
+  centroids(i,:)=mean(b);
+end
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
